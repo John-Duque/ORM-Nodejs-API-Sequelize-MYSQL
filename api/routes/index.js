0ca08b42e01f92ae282f860699015a51219b4ca4
John Duque <duquejohn185@gmail.com>
2022-10-24 17:57:47 -0300

    subindo o swagger na aplicação

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 const pessoasRouter = require('./pessoasRoutes.js');
 const niveisRouter = require('./niveisRoutes.js');
 const turmas = require('./turmasRoutes.js');
 const matriculasRoutes = require('./matriculasRoutes.js');
 
-
-module.exports = app =>{
+module.exports = app => {
     app.use(express.json());
     app.use(pessoasRouter); // Estou passando todas as rotas pessoa aqui para serem usadas
     app.use(niveisRouter);
     app.use(turmas);
     app.use(matriculasRoutes);
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
